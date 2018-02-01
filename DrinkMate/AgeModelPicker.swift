a21bf940113806084b3e5f442a8dd545fc748bcc
Djurre Hoeksema <dhoeksema1@live.maryville.edu>
2018-01-31 19:29:01 -0600

    DrinkMate Added

---
+++ DrinkMate/AgeModelPicker.swift
@@ -0,0 +1,21 @@
+//
+//  AgeModelPicker.swift
+//  DrinkMate
+//
+//  Created by Djurre Hoeksema on 1/30/18.
+//  Copyright © 2018 Maryville App Development. All rights reserved.
+//
+
+import UIKit
+
+class AgeModelPicker: UIPickerView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- DrinkMate/ViewController.swift
+++ DrinkMate/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var twoOzButton: UIButton!
+    @IBOutlet weak var fourOzButton: UIButton!
+    @IBOutlet weak var eightOzButton: UIButton!
+    @IBOutlet weak var twelfOZButton: UIButton!
+    @IBOutlet weak var sixteenOzButton: UIButton!
+    @IBOutlet weak var thirtyTwoOzButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func twoOzButttonPressed(_ sender: UIButton) {
+    
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
